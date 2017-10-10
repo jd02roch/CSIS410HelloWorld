6c03172ff0aba007cb9794f78eeb5b917e973eb5
jd02roch <31904324+jd02roch@users.noreply.github.com>
2017-10-10 18:15:47 -0400

    Add files via upload

---
+++ HelloWorld.java/Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
